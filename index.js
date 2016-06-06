a53d86303541a27139495d7185760f3184ecd1c5
Sebastian Bensusan <sbensu@gmail.com>
2016-06-06 08:30:59 -0700

    added some restaurants

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ require("clojurescript/register")
 
 const app = require('express')();
 
-const helper = require("helpers");
+const restaurants = [
+    {
+        "name": "Palmyra",
+        "type": "Mediterranean",
+        "url": "https://www.orderaheadapp.com/places/palmyra--san-francisco-ca"
+    },
+    {
+        "name": "Krua Thai",
+        "type": "Thai",
+        "url": "https://www.orderaheadapp.com/places/krua-thai--san-francisco-ca"
+    },
+    {
+        "name": "Pancho Villa",
+        "type": "Mexican"
+        "url": "https://www.orderaheadapp.com/places/pancho-villa-taqueria-san-francisco--san-francisco-ca"
+    },
+    {
+        "name": "707 Sutter",
+        "type": "Korean",
+        "url": "https://www.orderaheadapp.com/places/707-sutter--san-francisco-ca"
+    }
+];
 
 app.get('/', (req, res) => {
-    res.send(helpers.message());
+    res.send("Lunch Bot");
 });
 app.listen();
