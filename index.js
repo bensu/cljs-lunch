66e2f795e64222258a64fd4849a395b685084dfa
Sebastian Bensusan <sbensu@gmail.com>
2016-06-06 09:19:47 -0700

    make slack requests

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-
-const config = require('./config.json');
-
+const request = require('request');
 const app = require('express')();
 
-const restaurants = [
-    {
-        "name": "Palmyra",
-        "type": "Mediterranean",
-        "url": "https://www.orderaheadapp.com/places/palmyra--san-francisco-ca"
-    },
-    {
-        "name": "Krua Thai",
-        "type": "Thai",
-        "url": "https://www.orderaheadapp.com/places/krua-thai--san-francisco-ca"
-    },
-    {
-        "name": "Pancho Villa",
-        "type": "Mexican",
-        "url": "https://www.orderaheadapp.com/places/pancho-villa-taqueria-san-francisco--san-francisco-ca"
-    },
-    {
-        "name": "707 Sutter",
-        "type": "Korean",
-        "url": "https://www.orderaheadapp.com/places/707-sutter--san-francisco-ca"
+const config = require('./config.json');
+const restaurants = require('./restaurants.json');
+
+function slackRequest(text) {
+    return {
+	      url: config.SLACK_WEBHOOK_URL,
+	      method: 'POST',
+	      body: {
+            text: text,
+            username: "charlie-lunch",
+            channel: "#hooktest"
+        },
+	      json: true
     }
-];
+};
+
+function lunchSuggestion(date) {
+    const today = date.getDay();
+    const restaurant = restaurants[today];
+    const text = "@here Hey guys, today we should eat at " + restaurant.name +
+          ": <" + restaurant.url + ">";
+    return slackRequest(text);
+}
 
 app.get('/', (req, res) => {
-    res.send(config.SLACK_WEBHOOK_URL);
+    const date = new Date();
+    request(lunchSuggestion(date), function(error, response, body) {
+        res.send("I tried!");
+    });
 });
 app.listen(8888);
